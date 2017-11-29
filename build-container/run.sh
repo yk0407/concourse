386ba41f5e425fb107620d46e56cbeef345ec459
大崎　悠生 <osaki.yuki@rbizsol.com>
2017-11-29 20:19:45 +0900

    test

--- build-container/run.sh
+++ build-container/run.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-GIT_VARS=$(cd git-master && git log -n 1 --pretty=format:"%h %cn %ce %cd" --date=iso)
+GIT_VARS=$(cd git-master && git log -n 1 --pretty=format:"%h::%cn::%ce::%cd" --date=iso)
 
-# git archive --format=tar.gz HEAD > ~/git-container/master/mdsystem.tar.gz
+mkdir -p vars/git
+echo $GIT_VARS | awk -F'::' '{print $1}' > vars/git/hash
+echo $GIT_VARS | awk -F'::' '{print $2}' > vars/git/user
+echo $GIT_VARS | awk -F'::' '{print $3}' > vars/git/address
 
-echo $GIT_VARS
+#git archive --format=tar.gz HEAD > ~/git-container/master/mdsystem.tar.gz
