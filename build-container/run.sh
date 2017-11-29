75055565b1c0948219864d71032001a4cb862a08
大崎　悠生 <osaki.yuki@rbizsol.com>
2017-11-29 20:11:53 +0900

    test

--- build-container/run.sh
+++ build-container/run.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-echo 'Hello World'
+GIT_VARS=$(git log -n 1 --pretty=format:"%h %cn %ce %cd" --date=iso)
+
+# git archive --format=tar.gz HEAD > ~/git-container/master/mdsystem.tar.gz
+
+echo $GIT_VARS
